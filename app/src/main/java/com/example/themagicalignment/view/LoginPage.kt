362f5cdba7f621d800f0e34f4734dd6a291e5e78
KaMikadzeZi <93952053+kamikadzezi@users.noreply.github.com>
2024-10-28 12:36:54 +0300

    create MVVM LoginPage

--- app/src/main/java/com/example/themagicalignment/view/LoginPage.kt
+++ app/src/main/java/com/example/themagicalignment/view/LoginPage.kt
@@ -1,6 +1,5 @@
 package com.example.themagicalignment.view
 
-import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,58 +9,90 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.themagicalignment.R
 import com.example.themagicalignment.ui.theme.BackgroundBlack
 import com.example.themagicalignment.viewmodel.LoginViewModel
-import kotlinx.coroutines.flow.internal.NoOpContinuation.context
-import kotlin.coroutines.jvm.internal.CompletedContinuation.context
-import kotlin.math.log
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun LoginPage(modifier: Modifier = Modifier, viewModel: LoginViewModel) {
+    val context = LocalContext.current
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    val focusManager = LocalFocusManager.current
+    Scaffold(snackbarHost = { SnackbarHost(snackbarHostState) }) {
         Column(
             modifier = modifier
                 .fillMaxSize()
                 .background(color = BackgroundBlack)
-                .padding(horizontal = 24.dp),
+                .padding(horizontal = 24.dp)
+                .padding(it),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
 
         OutlinedTextField(value = viewModel.login.value , onValueChange = viewModel::onLoginUpdate,Modifier.fillMaxWidth())
         Spacer(Modifier.height(16.dp))
-        OutlinedTextField(value = viewModel.password.value , onValueChange = viewModel::onPasswordUpdate,Modifier.fillMaxWidth())
+            OutlinedTextField(
+                value = viewModel.password.value,
+                onValueChange = viewModel::onPasswordUpdate,
+                Modifier.fillMaxWidth(),
+                visualTransformation = if (viewModel.passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    val image = if (viewModel.passwordVisible.value) {
+                        painterResource(R.drawable.baseline_visibility_24)
+                    } else {
+                        painterResource(R.drawable.baseline_visibility_off_24)
+                    }
+                    IconButton(onClick = { viewModel.onPasswordVisibleUpdate() }) {
+                        Icon(painter = image, "")
+                    }
+                })
+            Spacer(Modifier.height(16.dp))
         Button(onClick = {
-        if (viewModel::userDataVerification.isFinal){
-
-        }    else {
+            if (viewModel.login.value.isNotBlank() && viewModel.password.value.isNotBlank()) {
+                focusManager.clearFocus()
+                if (viewModel.userDataVerification()) {
+                    scope.launch {
+                        snackbarHostState.showSnackbar("Ok", actionLabel = "ok")
+                    }
+                } else {
+                    scope.launch {
+                        snackbarHostState.showSnackbar("Не ок", actionLabel = "ok")
+                    }
+                }
+            } else {
             Toast.makeText(
-                get,
+                context,
                 "Пожалуйста введите логин и пароль",
                 Toast.LENGTH_SHORT
             ).show()
             }
-
         },Modifier.fillMaxWidth()) {Text(text = "Войти")}
-
-
         }
-
-
     }
+}
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/themagicalignment/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/themagicalignment/viewmodel/LoginViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.themagicalignment.viewmodel
 
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -16,15 +20,19 @@ class LoginViewModel : ViewModel(){
     val login : State<String> = _login
     private val _password = mutableStateOf("")
     val password : State<String> = _password
+    private val _passwordVisible = mutableStateOf(false)
+    val passwordVisible : State<Boolean> = _passwordVisible
     fun onLoginUpdate(value:String) {
         _login.value = value
     }
     fun onPasswordUpdate(value: String){
         _password.value = value
     }
+    fun onPasswordVisibleUpdate(){
+        _passwordVisible.value = !_passwordVisible.value
+    }
     fun userDataVerification(): Boolean{
-         return userRepository.fetchUserData().login==_login.value && userRepository.fetchUserData().password==_password.value
-
+         return userRepository.fetchUserData().login==_login.value.trim() && userRepository.fetchUserData().password==_password.value.trim()
     }
 
 }
